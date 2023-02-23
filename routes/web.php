085019b769409a57ca60b3b794d1b3abb57edc47
AlessandroLembo <alessandrolembo2@gmail.com>
2023-02-23 15:38:36 +0100

    create route comic

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/comics', function () {
+    return view('comics');
+});
+
+Route::get('/comic', function () {
+    return view('comic');
 });
